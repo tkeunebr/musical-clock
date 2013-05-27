63f19c6e850965396831f010c68d324157e139b5
Thomas <thomas.keunebroek@gmail.com>
2013-05-27 18:46:09 +0200

    Implement basic alarm mechanism

--- MusicalClock/ic07-musical-clock/src/main/java/fr/tkeunebr/ic07/MainActivity.java
+++ MusicalClock/ic07-musical-clock/src/main/java/fr/tkeunebr/ic07/MainActivity.java
@@ -1,17 +1,27 @@
 package fr.tkeunebr.ic07;
 
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+	private static final int DEFAULT_DURATION_SECONDS = 5;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		final Intent intent = new Intent(this, AlarmReceiver.class);
+		final PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 234324243, intent, 0);
+		final AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+		alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + (DEFAULT_DURATION_SECONDS * 1000), pendingIntent);
+		Toast.makeText(this, "Alarm set in " + DEFAULT_DURATION_SECONDS + " seconds", Toast.LENGTH_LONG).show();
 	}
 
 	@Override
