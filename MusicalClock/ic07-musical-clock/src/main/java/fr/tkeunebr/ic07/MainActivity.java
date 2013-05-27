1cf2bcea2e8262bdf9ba1a213cff465cdbaea599
Thomas <thomas.keunebroek@gmail.com>
2013-05-27 19:05:23 +0200

    Add an extremely basic alarm clock manager

--- MusicalClock/ic07-musical-clock/src/main/java/fr/tkeunebr/ic07/MainActivity.java
+++ MusicalClock/ic07-musical-clock/src/main/java/fr/tkeunebr/ic07/MainActivity.java
@@ -3,25 +3,44 @@ package fr.tkeunebr.ic07;
 import android.app.Activity;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
+import android.app.TimePickerDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
+import java.util.Calendar;
+
 public class MainActivity extends Activity {
-	private static final int DEFAULT_DURATION_SECONDS = 5;
+	private final TimePickerDialog.OnTimeSetListener mOnTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+		@Override
+		public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
+			Calendar calNow = Calendar.getInstance();
+			Calendar calSet = (Calendar) calNow.clone();
+
+			calSet.set(Calendar.HOUR_OF_DAY, hourOfDay);
+			calSet.set(Calendar.MINUTE, minute);
+			calSet.set(Calendar.SECOND, 0);
+			calSet.set(Calendar.MILLISECOND, 0);
+
+			if (calSet.compareTo(calNow) <= 0) {
+				//Today Set time passed, count to tomorrow
+				calSet.add(Calendar.DATE, 1);
+			}
+
+			setAlarm(calSet);
+		}
+	};
+	private TimePickerDialog mTimePickerDialog;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-		final Intent intent = new Intent(this, AlarmReceiver.class);
-		final PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 234324243, intent, 0);
-		final AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
-		alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + (DEFAULT_DURATION_SECONDS * 1000), pendingIntent);
-		Toast.makeText(this, "Alarm set in " + DEFAULT_DURATION_SECONDS + " seconds", Toast.LENGTH_LONG).show();
 	}
 
 	@Override
@@ -38,5 +57,27 @@ public class MainActivity extends Activity {
 				return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+
+	public void openAlarm(View v) {
+		Calendar calendar = Calendar.getInstance();
+		if (mTimePickerDialog == null) {
+			mTimePickerDialog = new TimePickerDialog(
+					this,
+					mOnTimeSetListener,
+					calendar.get(Calendar.HOUR_OF_DAY),
+					calendar.get(Calendar.MINUTE),
+					true);
+		}
+		mTimePickerDialog.setTitle("Set Alarm Time");
+		mTimePickerDialog.show();
+	}
+
+	private void setAlarm(Calendar targetCal) {
+		Toast.makeText(this, "Alarm is set@ " + targetCal.getTime(), Toast.LENGTH_LONG).show();
+		Intent intent = new Intent(getBaseContext(), AlarmReceiver.class);
+		PendingIntent pendingIntent = PendingIntent.getBroadcast(getBaseContext(), 1, intent, 0);
+		AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+		alarmManager.set(AlarmManager.RTC_WAKEUP, targetCal.getTimeInMillis(), pendingIntent);
 	}
 }
