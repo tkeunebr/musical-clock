1c250bde89e3a32e5bacd4ead46255a0c88f250a
Thomas <thomas.keunebroek@gmail.com>
2013-05-30 16:42:26 +0200

    Use both date and time to setup the alarm

--- MusicalClock/ic07-musical-clock/src/main/java/fr/tkeunebr/ic07/MainActivity.java
+++ MusicalClock/ic07-musical-clock/src/main/java/fr/tkeunebr/ic07/MainActivity.java
@@ -17,55 +17,50 @@ import android.widget.Toast;
 import java.util.Calendar;
 
 public class MainActivity extends Activity {
-	private DatePicker mDatePicker;
-	private TimePicker mTimePicker;
+    private DatePicker mDatePicker;
+    private TimePicker mTimePicker;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-		setContentView(R.layout.activity_main);
-		mDatePicker = (DatePicker) findViewById(R.id.date_picker);
-		mTimePicker = (TimePicker) findViewById(R.id.time_picker);
-		mTimePicker.setIs24HourView(DateFormat.is24HourFormat(this));
-		mTimePicker.setCurrentHour(Calendar.getInstance().get(Calendar.HOUR_OF_DAY));
-	}
+        setContentView(R.layout.activity_main);
+        mDatePicker = (DatePicker) findViewById(R.id.date_picker);
+        mTimePicker = (TimePicker) findViewById(R.id.time_picker);
+        mTimePicker.setIs24HourView(DateFormat.is24HourFormat(this));
+        mTimePicker.setCurrentHour(Calendar.getInstance().get(Calendar.HOUR_OF_DAY));
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-			case R.id.action_settings:
-				startActivity(new Intent(this, PreferencesActivity.class));
-				return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                startActivity(new Intent(this, PreferencesActivity.class));
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
-	public void setAlarm(View v) {
-		Calendar calNow = Calendar.getInstance();
-		Calendar calSet = (Calendar) calNow.clone();
+    public void setAlarm(View v) {
+        final Calendar calSet = Calendar.getInstance();
 
-		calSet.set(Calendar.HOUR_OF_DAY, mTimePicker.getCurrentHour());
-		calSet.set(Calendar.MINUTE, mTimePicker.getCurrentMinute());
-		calSet.set(Calendar.SECOND, 0);
-		calSet.set(Calendar.MILLISECOND, 0);
+        calSet.set(Calendar.YEAR, mDatePicker.getYear());
+        calSet.set(Calendar.MONTH, mDatePicker.getMonth());
+        calSet.set(Calendar.DAY_OF_MONTH, mDatePicker.getDayOfMonth());
+        calSet.set(Calendar.HOUR_OF_DAY, mTimePicker.getCurrentHour());
+        calSet.set(Calendar.MINUTE, mTimePicker.getCurrentMinute());
+        calSet.set(Calendar.SECOND, 0);
+        calSet.set(Calendar.MILLISECOND, 0);
 
-		if (calSet.compareTo(calNow) <= 0) {
-			//Today Set time passed, count to tomorrow
-			calSet.add(Calendar.DATE, 1);
-		}
-
-		// TODO: use date + time to set alarm
-		Toast.makeText(this, "Alarm is set@ " + calSet.getTime(), Toast.LENGTH_SHORT).show();
-		Intent intent = new Intent(getBaseContext(), AlarmReceiver.class);
-		PendingIntent pendingIntent = PendingIntent.getBroadcast(getBaseContext(), 1, intent, 0);
-		AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-		alarmManager.set(AlarmManager.RTC_WAKEUP, calSet.getTimeInMillis(), pendingIntent);
-	}
+        Toast.makeText(this, "Alarm is set the " + calSet.get(Calendar.DAY_OF_MONTH) + " @ " + calSet.getTime(), Toast.LENGTH_SHORT).show();
+        final AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        alarmManager.set(AlarmManager.RTC_WAKEUP, calSet.getTimeInMillis()
+                PendingIntent.getBroadcast(this, 1, new Intent(this, AlarmReceiver.class), 0));
+    }
 }
